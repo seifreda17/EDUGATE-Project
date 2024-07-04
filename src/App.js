71a284281486dfead44ec98b8c8ac7efce04d70e
seifreda17 <76448016+seifreda17@users.noreply.github.com>
2024-07-04 23:53:16 +0300

    Delete src directory

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import Pages from "./components/pages/Pages";
-import Header from "./components/common/header/Header";
-
-function App() {
-  return <Pages />;
-}
-
-export default App;
